b8e19a3719a716468a424af8e0226ca1ed8c4bd2
amhillwig <amhillwig@gmail.com>
2025-10-27 00:36:49 -0400

    scripts for animations and camera

--- Assets/_Scripts/RedController.cs
+++ Assets/_Scripts/RedController.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class RedController : MonoBehaviour
 {
     public float speed = 0.5f;
+    private bool isMoving = false;
+
+    private Animator animator;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,49 +20,72 @@ public class RedController : MonoBehaviour
         {
             // Create a new vector where we modify the x position
             // of our game object
+            isMoving = true;
             Vector2 pos = new Vector2(
-                gameObject.transform.position.x + speed,
+                gameObject.transform.position.x + speed * Time.deltaTime,
                 gameObject.transform.position.y);
 
             // Assign new position vector to game object
             gameObject.transform.position = pos;
+
+            animator.SetFloat("moveX", 1);
+            animator.SetFloat("moveY", 0);
 
         }
         else if (Input.GetKey(KeyCode.A))
         {
             // Create a new vector where we modify the x position
             // of our game object
+            isMoving = true;
             Vector2 pos = new Vector2(
-                gameObject.transform.position.x - speed,
+                gameObject.transform.position.x - speed * Time.deltaTime,
                 gameObject.transform.position.y);
 
             // Assign new position vector to game object
             gameObject.transform.position = pos;
+
+            animator.SetFloat("moveX", -1);
+            animator.SetFloat("moveY", 0);
 
         }
         else if (Input.GetKey(KeyCode.W))
         {
             // Create a new vector where we modify the x position
             // of our game object
+            isMoving = true;
             Vector2 pos = new Vector2(
                 gameObject.transform.position.x,
-                gameObject.transform.position.y + speed);
+                gameObject.transform.position.y + speed * Time.deltaTime);
 
             // Assign new position vector to game object
             gameObject.transform.position = pos;
+
+            animator.SetFloat("moveY", 1);
+            animator.SetFloat("moveX", 0);
 
         }
         else if (Input.GetKey(KeyCode.S))
         {
             // Create a new vector where we modify the x position
             // of our game object
+            isMoving = true;
             Vector2 pos = new Vector2(
                 gameObject.transform.position.x,
-                gameObject.transform.position.y - speed);
+                gameObject.transform.position.y - speed * Time.deltaTime);
 
             // Assign new position vector to game object
             gameObject.transform.position = pos;
 
+            animator.SetFloat("moveY", -1);
+            animator.SetFloat("moveX", -1);
+
+        } else
+        {
+            isMoving = false;
+            //animator.SetFloat("moveX", 0);
+            //animator.SetFloat("moveY", 0);
         }
+        
+        animator.SetBool("isMoving", isMoving);
     }
 }
