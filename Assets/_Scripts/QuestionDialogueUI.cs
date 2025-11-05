500dd947ba4252a22a842728f1745dedb4833c9a
maha300 <mahaalrifai@hotmail.de>
2025-11-04 21:03:54 -0500

    Dialogue on wolf and sandwich

--- Assets/_Scripts/QuestionDialogueUI.cs
+++ Assets/_Scripts/QuestionDialogueUI.cs
@@ -5,42 +5,44 @@ using TMPro;
 
 public class QuestionDialogueUI : MonoBehaviour
 {
+    public static QuestionDialogueUI Instance
+    {
+        get; private set;
+    }
     private TextMeshProUGUI textMeshPro;
     private Button yesButton;
     private Button noButton;
     private void Awake()
     {
+        Instance = this;
         textMeshPro = transform.Find("Text").GetComponent<TextMeshProUGUI>();
         yesButton = transform.Find("YesBtn").GetComponent<Button>();
         noButton = transform.Find("NoBtn").GetComponent<Button>();
 
-        ShowQuestion("Do you?", () =>
-        {
-            Debug.Log("Yes");
-        }, () =>
-        {
-            Debug.Log("No");
-        });
+        Hide();
         
 
     }
 
     public void ShowQuestion(string questionText, System.Action yesAction, System.Action noAction)
     {
+        gameObject.SetActive(true);
         textMeshPro.text = questionText;
+
+
         yesButton.onClick.AddListener(() =>
         {
             Hide();
-            yesAction();
+            yesAction?.Invoke();
         });
+
         noButton.onClick.AddListener(() =>
         {
             Hide();
-            noAction();
+            noAction?.Invoke();
         });
-
-
     }
+
     private void Hide()
     {
         gameObject.SetActive(false);
---
+++ Assets/_Scripts/SandwichDialogue.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SandwichDialogue : MonoBehaviour
+{
+    public GameObject parentObject;
+
+    private GameObject childObject;
+
+    void Start()
+    {
+        // Path to the nested child (Parent/Child/Grandchild)
+        Transform childTransform = parentObject.transform.Find("GameManager/UI/DialogueBox");
+
+        if (childTransform != null)
+        {
+            childObject = childTransform.gameObject;
+            Debug.Log("Found nested child: " + childObject.name);
+
+            // Hide the child
+            childObject.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("Nested child not found! Check the path.");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            
+            QuestionDialogueUI.Instance.ShowQuestion("Are you sure you want to give the wolf grandma's sandwich?", () => {
+                SceneManager.LoadScene("Forest");
+            }, () => {
+                SceneManager.LoadScene("Death");
+                });
+        }
+    }
+}
