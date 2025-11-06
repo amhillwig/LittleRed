da7527a963388ba94abcf268edb8f819b3f58f6a
amhillwig <106126618+amhillwig@users.noreply.github.com>
2025-11-06 02:33:53 -0500

    errors

--- Assets/_Scripts/QuestController.cs
+++ Assets/_Scripts/QuestController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class QuestController : MonoBehaviour
 {
     public static QuestController Instance { get; private set; }
-    public List<QuestProgress> activateQuests = new();
+    //public List<QuestProgress> activateQuests = new();
 
 
     private void Awake() {
@@ -14,13 +14,13 @@ public class QuestController : MonoBehaviour
     }
     public void AcceptQuest(QuestController quest)
     {
-        if (isQuestActive(quest.questID)) return;
+        //if (isQuestActive(quest.questID)) return;
 
-        activateQuests.Add(new QuestProgress(quest));
+        //activateQuests.Add(new QuestProgress(quest));
     }
-    public bool IsQuestActive(string questID) => activateQuests.Exists(QuestController => q.questID == questID);
+    //public bool IsQuestActive(string questID) => activateQuests.Exists(QuestController => q.questID == questID);
 
-    public bool IsWoodsmanQuest(string questID) => activateQuests.Exists(QuestController => q.questID == 1);
+    //public bool IsWoodsmanQuest(string questID) => activateQuests.Exists(QuestController => q.questID == 1);
     
 }
 
