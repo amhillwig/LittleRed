6c73699959d5d309b082250066b487e905779579
maha300 <mahaalrifai@hotmail.de>
2025-11-07 17:38:14 -0500

    Finalized Win Scene

---
+++ Assets/_Scripts/FinalQuestion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalQuestion : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+      
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            
+            QuestionDialogueUI.Instance.ShowQuestion("Give grandma the basket?", () => {
+                SceneManager.LoadScene("Menu");
+            }, () => {
+                
+                });
+        }
+    }
+}
